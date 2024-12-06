2545ddd7ac16bfa7cef4fbf6ab31590bf2a61470
Sairam <sairammekala7@gmail.com>
2024-12-06 12:26:42 +0530

    Added routes to forgot password page  and reset passsword page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import EditProfilePage from "./EditProfilePage";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Signup from "./Signup";
+import Resetpassword from "./Resetpass";
+import ForgotPassword from "./forgotpass";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         {/* <Route path="/" element={<ProfilePage />} /> */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<Resetpassword />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
